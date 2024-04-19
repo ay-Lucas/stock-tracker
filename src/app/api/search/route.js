4ec92171557c83695af006cd164797281c66222d
user <user@terminal.com>
2024-04-19 18:21:34 -0400

    init + yahoo finance api route

---
+++ src/app/api/search/route.js
@@ -0,0 +1,35 @@
+// export const dynamic = 'force-dynamic' // defaults to auto
+// /api/stock?{ticker}
+export async function GET(request) {
+  const searchParams = new URLSearchParams(request.url);
+  const ticker = searchParams.get("ticker");
+  const range = searchParams.get("range");
+  const interval = searchParams.get("interval");
+  console.log(searchParams);
+
+  if (!ticker || !range || !interval)
+    return Response.json({ message: "missing parameters" }, { status: 400 });
+
+  const baseUrl = "https://query2.finance.yahoo.com/v8/finance"
+  const url = `${baseUrl}/chart/${ticker}?range=${range}&interval=${interval}`;
+  const response = await fetch(url);
+  if (!response.ok)
+    throw new Error("Failed to fetch data");
+  const data = await response.json();
+  // console.log(response.json().chart.result[0].indicators);
+  return Response.json(data);
+  // const getStock = (json) => {
+  //   let prices = [];
+  //   const data = json.chart.result[0];
+  //   for (let i = 0; i < data.timestamp.length; i++) {
+  //     let utc = new Date(0);
+  //     utc.setUTCSeconds(data.timestamp[i]);
+  //     const time = utc.toLocaleDateString();
+  //     const price = data.indicators.quote[0].close[i].toFixed(1);
+  //     prices.push(Quote("AAPL", price, time));
+  //     // prices.push(price);
+  //     // console.log(`${time}: ${price}\n`);
+  //   }
+  //   return prices;
+  // }
+}
---
+++ src/app/page.js
@@ -0,0 +1,45 @@
+// "use client";
+// import { Play } from "next/font/google";
+import Image from "next/image";
+// import { useEffect, useState } from "react";
+
+const Quote = (ticker, price, timestamp) => { return { ticker: ticker, price: price, timestamp: timestamp } }
+const getStock = (json) => {
+  let prices = [];
+  const data = json.chart.result[0];
+  for (let i = 0; i < data.timestamp.length; i++) {
+    let utc = new Date(0);
+    utc.setUTCSeconds(data.timestamp[i]);
+    const time = utc.toLocaleDateString();
+    const price = data.indicators.quote[0].close[i].toFixed(1);
+    prices.push(Quote("AAPL", price, time));
+    // prices.push(price);
+    // console.log(`${time}: ${price}\n`);
+  }
+  return prices;
+}
+
+export default async function Home() {
+  // const [data, setData] = useState(0);
+  // useEffect(() => {
+  //   setData(getData());
+  // }, []);
+  const ticker = "AAPL";
+  const range = "5d";
+  const interval = "1d";
+  console.log(process.env.REACT_APP_URL);
+  // const request = await fetch(`${process.env.REACT_APP_URL}/api/search/${ticker}?range=${range}&interval=${interval}`);
+  // const data = await request.json();
+
+
+  // const stock = getStock(data);
+  // for (let i = 0; i < stock.length; i++) {
+  //   console.log(stock[i]);
+  // }
+  // console.log(data.chart.result[0].indicators);
+  return (
+
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    </main>
+  );
+}
